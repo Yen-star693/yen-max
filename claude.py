a07dc91363ed10dbd4e22b2d8af061be6ae2a395
Yen-star693 <umajiniyambem400@gmail.com>
2026-07-30 19:11:37 +0530

    Update claude.py

--- claude.py
+++ claude.py
@@ -1,1 +1,29 @@
-ye
+from anthropic import Anthropic
+
+from config import (
+    ANTHROPIC_API_KEY,
+    CLAUDE_MODEL
+)
+
+from prompts import SYSTEM_PROMPT
+
+client = Anthropic(
+    api_key=ANTHROPIC_API_KEY
+)
+
+
+def ask_claude(prompt):
+
+    response = client.messages.create(
+        model=CLAUDE_MODEL,
+        system=SYSTEM_PROMPT,
+        max_tokens=700,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return response.content[0].text
